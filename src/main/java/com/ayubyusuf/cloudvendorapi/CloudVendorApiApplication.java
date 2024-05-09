eb9a7e2557a30cef7f08d535c2d300ecac659cf6
ayubyusuf <pw2712gz@go.minnstate.edu>
2024-05-09 12:06:05 -0500

    Updated MySQL database password in application properties and removed unnecessary whitespace in CloudVendorApiApplication.java

--- src/main/java/com/ayubyusuf/cloudvendorapi/CloudVendorApiApplication.java
+++ src/main/java/com/ayubyusuf/cloudvendorapi/CloudVendorApiApplication.java
@@ -2,12 +2,10 @@ package com.ayubyusuf.cloudvendorapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CloudVendorApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudVendorApiApplication.class, args);
     }
-
 }
